50cadf08fea3c2f733092c04f269a1483fa54ece
Dzianis Jackievič <den.overtone@gmail.com>
2019-01-01 14:08:50 +0300

    init layout and css skeleton

--- assets/ResourcesAsset.php
+++ assets/ResourcesAsset.php
@@ -10,22 +10,14 @@ class ResourcesAsset extends \luya\web\Asset
     public $sourcePath = '@app/resources';
     
     public $css = [
-        'css/style.css'
+        'css/normalize.css',
+        'css/skeleton.css',
     ];
 
     public $js = [
     ];
     
-    public $publishOptions = [
-        'only' => [
-            'css/*',
-            'js/*',
-        ]
-    ];
-
-
     public $depends = [
         'yii\web\JqueryAsset',
-        'app\assets\TempAsset',
     ];
 }
