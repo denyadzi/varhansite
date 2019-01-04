3a0cffa1cc3351f0bc1cf1b1273601dae1122df5
Dzianis Jackievič <den.overtone@gmail.com>
2019-01-04 09:20:01 +0300

    apply little bem

--- views/blocks/NavigationBlock.php
+++ views/blocks/NavigationBlock.php
@@ -14,12 +14,16 @@ use app\assets\blocks\NavigationAsset;
 NavigationAsset::register($this);
 ?>
 <nav class="navigation-block">
-    <ul>
+    <ul class="navigation-block__list">
 	<?php foreach (Yii::$app->menu->findAll(['parent_nav_id' => 0, 'container' => 'default']) as $item): ?>
-	    <li><a href="<?=$item->link?>"><?=$item->title?></a></li>
+	    <li class="navigation-block__list-item">
+		<a class="navigation-block__link" href="<?=$item->link?>"><?=$item->title?></a>
+	    </li>
 	    <?php if ($item->isHome): ?>
 		<?php foreach (Hook::iterate(Constants::HOOK_NAVIGATION_INJECT_AFTER_HOME) as $href => $title): ?>
-		    <li><a href="<?=$href?>"><?=$title?></a></li>
+		    <li class="navigation-block__list-item">
+			<a class="navigation-block__link" href="<?=$href?>"><?=$title?></a>
+		    </li>
 		<?php endforeach ?>
 	    <?php endif ?>
 	<?php endforeach ?>
