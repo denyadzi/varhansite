1bbba282be194a53215b875271cb92a01f48d924
Dzianis Jackievič <den.overtone@gmail.com>
2019-01-21 12:34:07 +0300

    Часовая замена віджэту LangSwitcher з-за багу з composition-кампанентам

--- views/cmslayouts/big-header.php
+++ views/cmslayouts/big-header.php
@@ -5,6 +5,7 @@ use luya\cms\widgets\LangSwitcher;
 ?>
 <header class="site-header site-header--big" style="background-image: url(<?=Hook::string(Constants::HOOK_HEADER_BG_IMAGE)?>)">
     <div class="site-languages--big-header">
+    <!--
 	<?=LangSwitcher::widget([
 	    'listElementOptions' => ['class' => 'site-languages--big-header__list'],
 	    'elementOptions' => ['class' => 'site-languages--big-header__item'],
@@ -12,7 +13,14 @@ use luya\cms\widgets\LangSwitcher;
 	    'linkLabel' => function($lang) {
 		return mb_strtoupper (mb_substr ($lang['name'], 0, 3));
 	    },
-	]);?>
+	 ]);?>-->
+        <ul class="site-languages--big-header__list">
+            <?php foreach (['en', 'ru', 'by'] as $langCode): ?>
+                <li class="site-languages--big-header__item">
+                    <a class="site-languages--big-header__link" href="<?=$langCode == 'by' ? '/' : "/$langCode/homepage"?>"><?=mb_strtoupper ($langCode)?></a>
+                </li>
+        <?php endforeach ?>
+        </ul>
     </div>
     <div class="site-header-content site-header-content--big">
 	<?=$placeholders['content']?>
